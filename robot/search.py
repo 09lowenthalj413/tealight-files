ff4fe0d571212dd4cdddc64e6767a54aec919c59
09lowenthalj413 <09lowenthalj413@mysandstorm.org>
2015-08-18 15:05:02 +0100

    Closing robot/search.py

--- robot/search.py
+++ robot/search.py
@@ -7,14 +7,4 @@ from tealight.robot import (move,
                             right_side)
 from random import *
 
-last = int(smell())
-while True:
-  if touch() == "wall":
-    turn(2)
-  
-  move()
-  if last > int(smell()):
-    turn(randint(-2, 2))
-  last = int(smell())
-
 # map seen 'no fruit' sight lines
