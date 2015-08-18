4c98b2e471d6874e647e90ede26258cb8a33e125
09lowenthalj413 <09lowenthalj413@mysandstorm.org>
2015-08-18 11:57:33 +0100

    Running logo/triangle_fractal.py

--- logo/triangle_fractal.py
+++ logo/triangle_fractal.py
@@ -10,10 +10,10 @@ def trifrac(scale):
   if scale == 1:
     return
   
-  tri(scale*10)
+  tri(scale)
   for i in range(3):
-    move(scale*10)
+    move(scale)
     turn(120)
-    trifrac(scale*5)
+    trifrac(scale/2)
     
 trifrac(100)
