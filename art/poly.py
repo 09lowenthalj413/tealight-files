099040711f9f175376eb00f50617318dfb3eb993
09lowenthalj413 <09lowenthalj413@users.noreply.github.com>
2015-08-20 10:47:21 +0100

    Running art/poly.py

--- art/poly.py
+++ art/poly.py
@@ -1,1 +1,26 @@
-print "A new file!"
+from tealight.art import *
+
+from github.daviesian.logo.lib import lib_func
+
+
+c = [(100,100), (5,50), (200,300), (260, 50), (370, 400), (290,20), (280,50),(275,10)]
+fill_polygon(c)
+
+color("red")
+#line(50,60,1000,60)
+#spot(287.89,60,5)
+
+density = 5
+
+for i in range(0,500,density):
+  for j in range(0,500,density):  
+    if test_polygon(i,j,c):
+      color("rgba(255,0,0,0.5)")
+    else:
+      color("blue")
+    spot(i,j,2)    
+
+if __name__ == "__main__":
+  print "Test: " + str(test_polygon(50,50,c))
+
+  
