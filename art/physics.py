87ebd6891f4bdd95836489481a5b25084d007413
09lowenthalj413 <09lowenthalj413@users.noreply.github.com>
2015-08-20 10:54:07 +0100

    Running art/physics.py

--- art/physics.py
+++ art/physics.py
@@ -5,7 +5,7 @@ class Car:
   mesh = [(0, 60), (30, -20), (-30, -20)]
   angle = 0
   pos = (100, 100)
-  vel = (10, 0)
+  vel = (0.1, 0)
   
   def step(self):
     self.pos = (self.pos[0] + self.vel[0],
