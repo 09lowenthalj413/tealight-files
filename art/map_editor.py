b62b3ea6d48657d358bd706bb454ca279360d635
jmlowenthal <jmlowenthal@users.noreply.github.com>
2015-08-20 13:46:27 +0100

    Running art/map_editor.py

--- art/map_editor.py
+++ art/map_editor.py
@@ -1,1 +1,11 @@
-print "A new file!"
+from tealight.art import *
+
+poly = []
+def handle_mousedown(x, y):
+  poly.append((x, y))
+  
+  color("white")
+  box(0, 0, screen_width, screen_height)
+  
+  color("red")
+  fill_polygon(poly)
